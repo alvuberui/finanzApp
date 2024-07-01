389d799d63925504ec52bd9f093d5cdacdcf6478
unknown <alvaroubeda8@gmail.com>
2024-07-01 13:36:49 +0200

    Fix some mobile bugs

--- app/info/help/page.jsx
+++ app/info/help/page.jsx
@@ -6,7 +6,7 @@ const Page = () => {
       <div className="max-w-4xl w-full p-5">
         <h1 className="text-3xl font-bold mb-4">Contacto</h1>
         <p className="mb-4">
-          Puedes contactar con nosotros a través de nuestro correo electrónico: alvuberui@outlook.com
+          Puedes contactar con nosotros a través de nuestro correo electrónico: devfinanzapp@gmail.com
         </p>
       </div>
     </main>
